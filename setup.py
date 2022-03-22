54c4bb16c80fbb4fc5837d43dc935e146cad1ee3
Francisco Gonzalez <francisco.gonzalez@strong.io>
2022-03-22 09:49:06 -0500

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sesh",
     version="0.0.1",
-    license="",
+    url="https://github.com/panchgonzalez/aws-sesh",
     author="Francisco Gonzalez",
     author_email="fg@panch.io",
     description="AWS MFA session manager",
